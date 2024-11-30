40282a161a686fd71f50e61b2d232fc3f5f9cd5b
Kaung Htet San <kghtetsan26@gmail.com>
2024-11-30 22:10:22 +0630

    add new components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+import { BookList } from "./components/BookList";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    
-  </StrictMode>,
-)
+    <Navbar />
+    <Home />
+    <BookList />
+  </StrictMode>
+);
